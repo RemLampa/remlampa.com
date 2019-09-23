644a6f9072b45ba2112d540110439c8c34b94d2b
Rem Lampa <rem@remlampa.com>
2019-09-23 22:29:29 +0800

    Fix fontawesome icons :bug:

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,11 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+
 // custom typefaces
 import 'typeface-montserrat';
 import 'typeface-merriweather';
 
 import 'prismjs/themes/prism.css';
+
+import './node_modules/@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
